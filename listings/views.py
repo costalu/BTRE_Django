a1863297153ccd42dd8e057755a530ad85646112
Fernando <costalu@sheridancollege.ca>
2019-12-19 16:00:46 -0500

    dynamically adding data from database to the listings and listing pages. Added pagination to the listings page.

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    
+    listing = Listing.objects.get(id = listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
