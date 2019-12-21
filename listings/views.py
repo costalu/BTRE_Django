104b108d613dd5cb4cccde6c4d9e4ec9b4dc7dc9
Fernando <costalu@sheridancollege.ca>
2019-12-20 23:09:11 -0500

    able to use search box to filter listings from the home page and from the listings page. Options on search box not hard coded anymore. Created accounts app in order to work on user authentication.

--- listings/views.py
+++ listings/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import *
+
 
 def index(request):
-    
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 3)
+
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
@@ -28,4 +30,44 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    #Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            listings = listings.filter(description__icontains=keywords)
+
+    #City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            listings = listings.filter(city__iexact=city)
+
+    #State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            listings = listings.filter(state__iexact=state)
+
+    #Bedrooms
+    if 'bedrooms' in request.GET:
+        bedroom = request.GET['bedrooms']
+        if bedroom:
+            listings = listings.filter(bedrooms__lte=bedroom)
+
+    #Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            listings = listings.filter(price__lte=price)
+
+    context = {
+        'state_choices': states,
+        'bedroom_choices': bedrooms,
+        'price_choices': prices,
+        'listings': listings,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from listings.models import Realtor
+from listings.choices import *
 
 
 def index(request):
@@ -9,6 +10,9 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
+        'state_choices': states,
+        'bedroom_choices': bedrooms,
+        'price_choices': prices,
         'listings': listings
     }
 
